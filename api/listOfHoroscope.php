c51054f220ee88023ea220732660c626d5f23920
FredrikOlssons <fredrik_olssons@live.se>
2021-12-10 09:06:28 +0100

    Fixat llite med funktionerna

--- api/listOfHoroscope.php
+++ api/listOfHoroscope.php
@@ -1,45 +1,130 @@
 <?php
 
-    $horoscope array(
-        'name' => 'aries',
-        'start' => '03-21', 
-        'end' => '04-20'
-    ),
-    array('name' => 'taurus',
-        'key' => 1, 'unicode' => '♉', 'start' => '04-21', 'end' => '05-21'
-    ),
-    array('name' => 'gemini',
-        'key' => 2, 'unicode' => '♊', 'start' => '05-22', 'end' => '06-21'
-    ),
-    array('name' => 'cancer',
-        'key' => 3, 'unicode' => '♋', 'start' => '06-22', 'end' => '07-22'
-    ),
-    array('name' => 'leo',
-        'key' => 4, 'unicode' => '♌', 'start' => '07-23', 'end' => '08-23'
-    ),
-    array('name' => 'virgo',
-        'key' => 5, 'unicode' => '♍', 'start' => '08-24', 'end' => '09-23'
-    ),
-    array('name' => 'libra',
-        'key' => 6, 'unicode' => '♎', 'start' => '09-24', 'end' => '10-23'
-    ),
-    array('name' => 'scorpio',
-        'key' => 7, 'unicode' => '♏', 'start' => '10-24', 'end' => '11-22'
-    ),
-    array('name' => 'sagittarius',
-        'key' => 8, 'unicode' => ' ♐', 'start' => '11-23', 'end' => '12-21'
-    ),
-    array('name' => 'capricorn',
-        'key' => 9, 'unicode' => '♑', 'start' => '12-22', 'end' => '12-31'
-    ),
-    array('name' => 'aquarius',
-        'key' => 10, 'unicode' => '♒', 'start' => '01-21', 'end' => '02-19'
-    ),
-    array('name' => 'pisces',
-        'key' => 11, 'unicode' => '♓', 'start' => '02-20', 'end' => '03-20'
-    ),
-    array('name' => 'capricorn',
-        'key' => 9, 'unicode' => '♑', 'start' => '01-01', 'end' => '01-20'
-    ),
-);
+$horoscopeList = [
+    [
+        'name' => 'Väduren',
+        'start' => ["month" => 3,"day" => 21],
+        'end' => ["month" => 4,"day" => 20]
+    ],
+    [
+        'name' => 'Oxen',
+        'start' => ["month" => 4,"day" => 21],
+        'end' => ["month" => 5,"day" => 21],
+    ],
+    [
+        'name' => 'Tvillingarna',
+        'start' => ["month" => 5,"day" => 22],
+        'end' => ["month" => 6,"day" => 21],
+    ],
+    [
+        'name' => 'Kräftan',
+        'start' => ["month" => 6,"day" => 22],
+        'end' => ["month" => 7,"day" => 22],
+    ],
+    [
+        'name' => 'Lejonet',
+        'start' => ["month" => 7,"day" => 23],
+        'end' => ["month" => 8,"day" => 23],
+    ],
+    [
+        'name' => 'Jungfrun',
+        'start' => ["month" => 8,"day" => 24],
+        'end' => ["month" => 9,"day" => 22],
+    ],
+    [
+        'name' => 'Vågen',
+        'start' => ["month" => 9,"day" => 23],
+        'end' => ["month" => 10,"day" => 23],
+    ],
+    [
+        'name' => 'Skorpionen',
+        'start' => ["month" => 10,"day" => 24],
+        'end' => ["month" => 11,"day" => 22],
+    ],
+    [
+        'name' => 'Skytten',
+        'start' => ["month" => 11,"day" => 23],
+        'end' => ["month" => 12,"day" => 21],
+    ],
+    [
+        'name' => 'Stenbocken',
+        'start' => ["month" => 12,"day" => 22],
+        'end' => ["month" => 01,"day" => 20],
+    ],
+    [
+        'name' => 'Vattumannen',
+        'start' => ["month" => 1,"day" => 21],
+        'end' => ["month" => 2,"day" => 18],
+    ],
+    [
+        'name' => 'Fiskarna',
+        'start' => ["month" => 2,"day" => 19],
+        'end' => ["month" => 3,"day" => 20],
+    ],
+];
+
+function calculate($horoscopeList , $date) {
+
+    $date = ["month = 3, ']
+
+for ($i = 0; $i < count($horoscopeList); $i++) {
+    $horoscope = $horoscopeList[$i];
+
+   
+    $startMonth = $horoscope["start"]["month"];
+    $startDay = $horoscope["start"]["day"];
+    $endMonth = $horoscope["end"]["month"];
+    $endDay = $horoscope["end"]["day"];
+
+    if ($date == $startMonth, $startDay 
+
+
+    if ($startMonth === $date  3 && $startDay >= 21 && $endMonth <= 4 && $endDay <= 20) {
+        return 'Väduren';
+    }
+
+}
+}
+
+
 ?>
+
+function getsign($startMonth, $startDay, $endMonth, $endDay) {
+if(($startMonth==1 && $startDay>20)||($endMonth==2 && $endDay<19)) { $mysign="Vattumannen" ; } 
+if(($startMonth==2 && $startDay>18)||($endMonth==3 && $endDay<21)) { $mysign="Fiskarna" ; } 
+if(($startMonth==3 && $startDay>20)||($endMonth==4 && $endDay<21)) { $mysign="Väduren" ; } 
+if(($startMonth==4 && $startDay>21)||($endMonth==5 && $endDay<22)) { $mysign="Oxen" ; } 
+if(($startMonth==5 && $startDay>21)||($endMonth==6 && $endDay<22)) { $mysign="Tvillingarna" ; } 
+if(($startMonth==6 && $startDay>21)||($endMonth==7 && $endDay<23)) { $mysign="Kräftan" ; } 
+if(($startMonth==7 && $startDay>22)||($endMonth==8 && $endDay<24)) { $mysign="Lejon" ; } 
+if(($startMonth==8 && $startDay>23)||($endMonth==9 && $endDay<23)) { $mysign="Jungfru" ; } 
+if(($startMonth==9 && $startDay>22)||($endMonth==10 && $endDay<24)) { $mysign="Vågen" ; } 
+if(($startMonth==10 && $startDay>23)||($endMonth==11 && $endDay<23)) { $mysign="Skorpion" ; } 
+if(($startMonth==11 && $startDay>21)||($endMonth==12 && $endDay<21)) { $mysign="Skytten" ; } 
+if(($startMonth==12 && $startDay>21)||($endMonth==1 && $endDay<21)) { $mysign="Stenbock" ; } 
+
+
+
+
+
+
+
+
+
+
+
+return $mysign; } print_r(getsign(16,7));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,26 +1,70 @@
-window.addEventListener('load', initSite);
+window.addEventListener("load", initSite);
 
-async function initSite() {
-    let response = await fetch('./api/postHoroscope.php', {method: "POST"});
-    let result = await response.json();
-    console.log(response);
-    console.log(result);
-}
+document.getElementById("saveButton").addEventListener("click", saveHoroscope);
+document.getElementById("updateButton").addEventListener("click", updateHoroscope);
+document.getElementById("deleteButton").addEventListener("click", deleteHoroscope);
 
-function makeRequest(url){
- 
-    fetch(url)
-        
-        then((response) => {
-        return response.json()
-    }).then((result) => {
-        console.log(result)
-        callback(result)
-    }).catch((err) => {
-        console.log("Error : ",err); 
-    })
+async function initSite() {}
+
+async function addResultInput() {
+    document.getElementById('addResult').innerText = result
+    let resultHoroscope = await makeRequest(url, body, option, addDate);
+    console.log(resultHoroscope)
 }
 
 
+async function saveHoroscope() {
+  let addHoroscope = document.getElementById("inputDate").value;
+  let date = new Date(addHoroscope);
+  let body = new FormData();
+  body.set("date", addHoroscope);
+  let option = {method: "POST" , body}
+  let result = await makeRequest("./api/addHoroscope.php", option);
+  let month = date.getMonth() +1
+  let day = date.getDate()
+  console.log(date)
+  //console.log(date.getMonth() +1, date.getDate( +1))
+  console.log(day)
+  console.log(month)
+  document.getElementById('addResult').innerText = month
+
+}
+
+async function deleteHoroscope() {
+  let deleteHoroscope = document.getElementById("inputDate").value;
+  let body = new FormData();
+  let option = {method: "DELETE" , body}
+  body.set(deleteHoroscope , "date");
+  let result = await makeRequest("./api/deleteHoroscope.php", option);
+  console.log(result)
+}
+
+  async function updateHoroscope() {
+    let updateHoroscope = document.getElementById("inputDate").value;
+    let body = new FormData();
+    let option = {method: "POST", body}
+    body.set(updateHoroscope, "date");
+    let result = await makeRequest("./api/updateHoroscope.php", option);
+    console.log(result)
+}
+
+  async function makeRequest(url, option) {
+    try {
+        let response = await fetch(url, option)
+        let result = response.json()
+        return result
+    } catch(err) {
+        console.error(err)
+    }
+  }
+
+  
 
 
+  
+/* 
+  formdata variabel
+  addeventlistener på knappar
+  funktion makerequest med url, olika för olika phps
+  !istället på andra iffen
+   */
