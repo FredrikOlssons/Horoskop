35b66384257c4339ab43fa517d0b93158eebccb7
FredrikOlssons <fredrik_olssons@live.se>
2021-12-17 17:57:07 +0100

    Funktioner färdiga

--- api/addHoroscope.php
+++ api/addHoroscope.php
@@ -7,21 +7,20 @@ try {
     if($_SERVER["REQUEST_METHOD"]) {
 
         if($_SERVER["REQUEST_METHOD"] == "POST") {
-            //    if (!isset($_SESSION["horoscope"])) {
-            $date = json_decode($_POST["date"], true);
-
-            $horoscope = getsign($horoscopeList, $date);
-
-            $_SESSION["horoscope"] = serialize($horoscope);
-
-            echo json_encode(true);
-            exit;
-            
-        } else {
-            echo json_encode(false);
-            echo json_encode("Not a POST-method");
-        }
-    
+            if (isset($_SESSION["horoscope"])) {
+                echo json_encode(false);
+                exit;
+            } else {
+                $date = json_decode($_POST["date"], true);
+                $horoscope = getsign($horoscopeList, $date);
+                $_SESSION["horoscope"] = serialize($horoscope);
+                echo json_encode(true);
+                exit;
+            }
+            } else {
+                echo json_encode(false);
+                echo json_encode("Not a POST-method");
+            }
     }
 } catch(Exception $err) {
     http_response_code($error->getcode);
--- main.js
+++ main.js
@@ -4,67 +4,90 @@ document.getElementById("saveButton").addEventListener("click", saveHoroscope);
 document.getElementById("updateButton").addEventListener("click", updateHoroscope);
 document.getElementById("deleteButton").addEventListener("click", deleteHoroscope);
 
-async function initSite() {}
-
-async function addResultInput() {
-    document.getElementById("addResult").innerText = result
-    let resultHoroscope = await makeRequest(url, body, option, addDate);
-    console.log(resultHoroscope)
+async function initSite() {
+  addResultInput();
 }
 
+async function addResultInput() {
+  try {
+    let resultHoroscope = await makeRequest("./api/viewHoroscope.php", {
+      method: "GET",
+    });
+    console.log(resultHoroscope)
+    if (resultHoroscope) {
+      document.getElementById("addResult").innerHTML = resultHoroscope;
+    } else {
+      document.getElementById("addResult").innerHTML = "";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function saveHoroscope() {
   let addHoroscope = document.getElementById("inputDate").value;
+  document.getElementById("inputDate").value = "";
+  if (!addHoroscope.length) {
+    console.log("Inget datum ifyllt");
+    alert("Var god fyll i ditt födelsedatum");
+    return;
+  }
+  console.log(addHoroscope);
   let date = new Date(addHoroscope);
   let body = new FormData();
-  let inputDate = {month: date.getMonth() +1, day: date.getDate()}
+  let inputDate = { month: date.getMonth() + 1, day: date.getDate() };
   body.set("date", JSON.stringify(inputDate));
-  let option = {method: "POST" , body:body}
+  let option = { method: "POST", body: body };
   let result = await makeRequest("./api/addHoroscope.php", option);
-  console.log(result)
-  
- 
- // document.getElementById('addResult').innerText = month'
- 
-
+  console.log(result);
+  addResultInput();
 }
 
 async function deleteHoroscope() {
-  let deleteHoroscope = document.getElementById("inputDate").value;
-  let option = {method: "DELETE"}
+  document.getElementById("inputDate").value = "";
+  let option = { method: "DELETE" };
   let result = await makeRequest("./api/deleteHoroscope.php", option);
-  console.log(result)
-  //console.log(deleteHoroscope)
+  console.log(result);
 
+  addResultInput();
 }
 
-  async function updateHoroscope() {
-    let updateHoroscope = document.getElementById("inputDate").value;
-    let body = new FormData();
-    let option = {method: "POST", body}
-    body.set(updateHoroscope, "date");
-    let result = await makeRequest("./api/updateHoroscope.php", option);
-    console.log(result)
-}
-
-  async function makeRequest(url, option) {
-    try {
-        let response = await fetch(url, option)
-        let result = await response.json()
-        return result
-
-    } catch(err) {
-        console.error(err)
-    }
+async function updateHoroscope() {
+  let updateHoroscope = document.getElementById("inputDate").value;
+  console.log(updateHoroscope)
+  
+  if (!updateHoroscope.length) {
+    console.log("Inget datum ifyllt");
+    alert("Var god fyll i ditt födelsedatum");
+    return;
   }
+  let date = new Date(updateHoroscope);
+  let body = new FormData();
+  let inputDate = { month: date.getMonth() + 1, day: date.getDate() };
+  body.set("date", JSON.stringify(inputDate));
 
+  let option = { method: "POST", body };
+  let result = await makeRequest("./api/updateHoroscope.php", option);
+  console.log(result);
+
+  if (result == true){
+    console.log(result)
+    addResultInput() 
+    
+  }
+    
+} 
   
+    
+  //knappen uppdatera ska skriva ut ett nytt stjärntecken
 
 
-  
-/* 
-  formdata variabel
-  addeventlistener på knappar
-  funktion makerequest med url, olika för olika phps
-  !istället på andra iffen
-   */
+async function makeRequest(url, option) {
+  try {
+    let response = await fetch(url, option);
+    let result = await response.json();
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
